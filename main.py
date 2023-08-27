e75414cc91a80544a83ae54e94ce002ec04394de
anfaly <142661981+anfaly@users.noreply.github.com>
2023-08-27 19:24:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-answer = ("anfal")
-answer = input("anfal")
-if answe == ("17")
-answer = input("17")
-print("my name is {anfal} and i am {17} year")
-
-
+my_name=input("enter your name")
+my_age=input("enter your age")
+print(f"my_name is {my_name} and i an {my_age} years old")
+first_number=int(input("enter first number"))
+second_number=int(input("enyer second number"))
+operation=input("what is the operation (+-*/)")
+if operation == '+' :
+    print(first_number + second_number)
+elif operation == "-" :
+    print(first_number - second_number)
+elif operation == "*" :
+    print(first_number * second_number)
+elif operation == "/" :
+    print(first_number / second_number)
